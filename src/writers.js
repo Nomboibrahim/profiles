a852cc2e74eba47904b5413eb9e557668a0ca4ac
nomboibrahim <nomboibrahim87@gmail.com>
2022-03-16 09:05:46 -0700

    first commit

---
+++ src/writers.js
@@ -0,0 +1,46 @@
+const writers=[
+{
+    id:1,
+    name: "jamila lorde",
+    avatar: "audre_lorde",
+    email: "jamila23@gmail.com",
+    phone: "0546282732",
+},
+{
+    id:2,
+    name: "jamila lorde",
+    avatar: "belle_hooks",
+    email: "jamila23@gmail.com",
+    phone: "0546282732",
+},
+{
+    id:3,
+    name: "jamila lorde",
+    avatar: "du_bois",
+    email: "jamila23@gmail.com",
+    phone: "0546282732",
+},
+{
+    id:4,
+    name: "jamila lorde",
+    avatar: "james_baldwin",
+    email: "jamila23@gmail.com",
+    phone: "0546282732",
+},
+{
+    id:5,
+    name: "jamila lorde",
+    avatar: "maya_angelou",
+    email: "jamila23@gmail.com",
+    phone: "0546282732",
+}
+
+]
+
+
+
+
+export default writers;
+
+
+
